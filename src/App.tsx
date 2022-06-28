c1640a2065e46b7798ccf479efbe249ef412be96
Asif <aumi.asif@gmail.com>
2022-06-28 22:06:43 +0600

    fourSquare api test

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { fourSquare } from "./httpService/httpService";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  const token = fourSquare?.token;
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: token,
+      },
+    };
+
+    fetch(
+      `https://api.foursquare.com/v3/places/search?v=20220626&query=restaurant`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }, [token]);
+  return <div className="App">hello</div>;
 }
 
 export default App;
---
+++ src/httpService/httpService.ts
@@ -0,0 +1,5 @@
+export const fourSquare = {
+  clientId: process.env.REACT_APP_FOURSQUARE_CLIENT_ID,
+  secretId: process.env.REACT_APP_FOURSQUARE_SECRET_ID,
+  token: `${process.env.REACT_APP_FOURSQUARE_OAUTH_TOKEN}`,
+};
